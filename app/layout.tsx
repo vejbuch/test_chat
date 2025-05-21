94eb94304b594c6735a855c7c743e83ccb48bd55
Nevim :) <vejbuch2@centrum.cz>
2025-05-21 19:05:39 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+// app/layout.tsx
+import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="cs">
+      <body>{children}</body>
+    </html>
+  );
+}
